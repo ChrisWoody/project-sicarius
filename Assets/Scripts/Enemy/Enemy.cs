1cf607f7306bcf596af2a22ff4ec26d7042e7159
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2018-01-14 14:35:21 +0800

    Add basic enemy and scripts

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Enemy : MonoBehaviour
+    {
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
