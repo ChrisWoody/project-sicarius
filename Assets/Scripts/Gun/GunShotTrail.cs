a719680537b86423f1b7b35568f4152e5e0c79a9
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2018-02-03 11:35:31 +0800

    Minor, finetune enemy speed plus gun shot trail

--- Assets/Scripts/Gun/GunShotTrail.cs
+++ Assets/Scripts/Gun/GunShotTrail.cs
@@ -42,9 +42,9 @@ namespace Assets.Scripts.Gun
 
         private static readonly Dictionary<Damage, float> DamageLineWidthMap = new Dictionary<Damage, float>
         {
-            {Damage.Low, 0.05f},
-            {Damage.Medium, 0.15f},
-            {Damage.High, 0.25f},
+            {Damage.Low, 0.03f},
+            {Damage.Medium, 0.09f},
+            {Damage.High, 0.15f},
         };
     }
 }
