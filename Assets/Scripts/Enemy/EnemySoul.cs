5592cea36850bb5637787379a50329cd846a8c2b
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2018-02-25 13:18:23 +0800

    Have particles flow from a collected enemy soul to the gun (to increase it's power), added some particle effects to the enemy soul and fade it out on collected

--- Assets/Scripts/Enemy/EnemySoul.cs
+++ Assets/Scripts/Enemy/EnemySoul.cs
@@ -1,27 +1,54 @@
-﻿using Assets.Scripts.Game;
+﻿using System.Collections;
+using Assets.Scripts.Game;
 using UnityEngine;
 
 namespace Assets.Scripts.Enemy
 {
     public class EnemySoul : MonoBehaviour
     {
+        public EnemySoulParticles EnemySoulParticles;
+
+        private SpriteRenderer _ps;
+
         private void Awake()
         {
-            GameController.OnRestartGame += DestroyEnemySoul;
+            GameController.OnRestartGame += () =>
+            {
+                DestoryOnRestart();
+                Destroy(gameObject);
+            };
+            _ps = GetComponent<SpriteRenderer>();
         }
 
-        private void DestroyEnemySoul()
+        private static void DestoryOnRestart()
         {
-            GameController.OnRestartGame -= DestroyEnemySoul;
-            Destroy(gameObject);
+            GameController.OnRestartGame -= DestoryOnRestart;
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.GetComponent<Player.Player>())
             {
-                GameController.NotifySoulCollected();
-                DestroyEnemySoul();
+                Instantiate(EnemySoulParticles, transform.position, transform.rotation);
+                GetComponent<ParticleSystem>().Stop(false, ParticleSystemStopBehavior.StopEmitting);
+                StartCoroutine(FadeOutTrail());
+                DestoryOnRestart();
+                Destroy(gameObject, DieInTime);
+            }
+        }
+
+        private const float DieInTime = 3f; // Time for the particle system to finish
+        private const float FadeOutSpeed = 0.05f;
+
+        private IEnumerator FadeOutTrail()
+        {
+            for (var i = 1f; i > -FadeOutSpeed; i -= FadeOutSpeed)
+            {
+                var color = _ps.color;
+                color.a = i;
+                _ps.color = color;
+
+                yield return new WaitForSeconds(FadeOutSpeed);
             }
         }
     }
---
+++ Assets/Scripts/Enemy/EnemySoulParticles.cs
@@ -0,0 +1,39 @@
+﻿using Assets.Scripts.Game;
+using UnityEngine;
+
+namespace Assets.Scripts.Enemy
+{
+    public class EnemySoulParticles : MonoBehaviour
+    {
+        private static Transform _gun;
+
+        private float _elapsed;
+        private bool _dead;
+        private const float TimeToTravel = 0.5f;
+
+        private void Start()
+        {
+            if (!_gun)
+                _gun = FindObjectOfType<Gun.Gun>().transform;
+        }
+
+        private void Update()
+        {
+            if (_dead)
+                return;
+
+            _elapsed += Time.deltaTime;
+            if (_elapsed >= TimeToTravel)
+            {
+                _dead = true;
+                GetComponent<ParticleSystem>().Stop(false, ParticleSystemStopBehavior.StopEmitting);
+                GameController.NotifySoulCollected();
+                Destroy(gameObject, 0.5f);
+            }
+
+            var toMove = Vector3.Lerp(transform.position, _gun.position, _elapsed / TimeToTravel);
+            toMove.z = 1f;
+            transform.position = toMove;
+        }
+    }
+}
