84848dfb2736cb6e090067451c32cfc77167725f
Chris Wood <16053164+ChrisWoody@users.noreply.github.com>
2018-04-15 11:19:42 +0800

    Preparing for intro, setup blend tree with dive/crouch/idle

---
+++ Assets/Scripts/Game/IntroManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Game
+{
+    public class IntroManager : MonoBehaviour
+    {
+        // Assumes that player is in correct starting position already
+
+        public Transform EndPoint;
+        public Transform Player;
+
+        public void PlayIntro()
+        {
+            //Player.GetComponent<Animator>().set
+            // show player
+        }
+
+        private void Update()
+        {
+            // set position of player
+        }
+    }
+}
